4506f5c5652556f365f3460325d7ca9eb000176d
cmanlh <cmanlh@163.com>
2017-01-30 21:51:08 +0800

    move monitor to another thread

--- src/main/java/com/lifeonwalden/springscheduling/task/Task.java
+++ src/main/java/com/lifeonwalden/springscheduling/task/Task.java
@@ -38,246 +38,260 @@ import com.lifeonwalden.springscheduling.monitor.TaskEvent;
 import com.lifeonwalden.springscheduling.monitor.TaskEventType;
 
 public abstract class Task implements Runnable, ScheduledFuture<Object> {
-    private final static Logger logger = LogManager.getLogger(DependentChainTask.class);
+  private final static Logger logger = LogManager.getLogger(Task.class);
 
-    protected String id;
+  protected String id;
 
-    protected String name;
+  protected String name;
 
-    protected Map<String, Object> param;
+  protected Map<String, Object> param;
 
-    protected TaskTriggerContext triggerContext;
+  protected TaskTriggerContext triggerContext;
 
-    protected Monitor monitor;
+  protected Monitor monitor;
 
-    protected boolean canRetry = false;
+  protected boolean canRetry = false;
 
-    /** always retry from the beginning of worker list */
-    protected boolean alwaysFromBeginning = false;
+  /** always retry from the beginning of worker list */
+  protected boolean alwaysFromBeginning = false;
 
-    /** retry after 30 Minutes */
-    protected long retryAfter = 1800;
+  /** retry after 30 Minutes */
+  protected long retryAfter = 1800;
 
-    protected ScheduledExecutorService executor;
+  protected ScheduledExecutorService executor;
 
-    protected TaskStatusEnum status = TaskStatusEnum.WAITING;
+  protected TaskStatusEnum status = TaskStatusEnum.WAITING;
 
-    private final Object triggerContextMonitor = new Object();
+  private final Object triggerContextMonitor = new Object();
 
-    private ScheduledFuture<?> currentFuture;
+  private ScheduledFuture<?> currentFuture;
 
-    public Task(String id, String name, TaskTriggerContext triggerContext) {
-        this.id = id;
-        this.name = name;
-        this.triggerContext = triggerContext;
+  public Task(String id, String name, TaskTriggerContext triggerContext) {
+    this.id = id;
+    this.name = name;
+    this.triggerContext = triggerContext;
+  }
+
+  public Task(String id, String name, TaskTriggerContext triggerContext, Monitor monitor) {
+    this.id = id;
+    this.name = name;
+    this.monitor = monitor;
+    this.triggerContext = triggerContext;
+  }
+
+  public Map<String, Object> getParam() {
+    return param;
+  }
+
+  public Task setParam(Map<String, Object> param) {
+    this.param = param;
+
+    return this;
+  }
+
+  public boolean isCanRetry() {
+    return canRetry;
+  }
+
+  public boolean isAlwaysFromBeginning() {
+    return alwaysFromBeginning;
+  }
+
+  public void setAlwaysFromBeginning(boolean alwaysFromBeginning) {
+    this.alwaysFromBeginning = alwaysFromBeginning;
+  }
+
+  public void setCanRetry(boolean canRetry) {
+    this.canRetry = canRetry;
+  }
+
+  public long getRetryAfter() {
+    return retryAfter;
+  }
+
+  /**
+   * retry after {retryAfter} seconds
+   * 
+   * @param retryAfter
+   */
+  public void setRetryAfter(long retryAfter) {
+    this.retryAfter = retryAfter;
+  }
+
+  public ScheduledExecutorService getExecutor() {
+    return executor;
+  }
+
+  public void setExecutor(ScheduledExecutorService executor) {
+    this.executor = executor;
+  }
+
+  public TaskStatusEnum getStatus() {
+    return status;
+  }
+
+  public Task setStatus(TaskStatusEnum status) {
+    this.status = status;
+
+    return this;
+  }
+
+  public String getId() {
+    return id;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public TaskTriggerContext getTriggerContext() {
+    return triggerContext;
+  }
+
+  public BaseTrigger getTrigger() {
+    return triggerContext.getTrigger();
+  }
+
+  public ScheduledFuture<?> schedule() {
+    synchronized (this.triggerContextMonitor) {
+      Date scheduledExecutionTime = this.triggerContext.getTrigger().nextExecutionTime(this.triggerContext);
+      if (scheduledExecutionTime == null) {
+        return null;
+      }
+      long initialDelay = scheduledExecutionTime.getTime() - System.currentTimeMillis();
+
+      logger.info("Schedule task [{}] run @ {}", this.name, scheduledExecutionTime.toString());
+      this.currentFuture = this.executor.schedule(this, initialDelay, TimeUnit.MILLISECONDS);
+      return this;
     }
+  }
 
-    public Task(String id, String name, TaskTriggerContext triggerContext, Monitor monitor) {
-        this.id = id;
-        this.name = name;
-        this.monitor = monitor;
-        this.triggerContext = triggerContext;
+  private void _run(Map<String, Object> param) {
+    Date nextExecutionTime = null, actualExecutionTime = null, completionTime = null;
+    List<String> failPrintList = null;
+    TaskEvent startTaskEvent = new TaskEvent();
+    final String taskId = this.id;
+    try {
+      Map<String, Object> _param =
+          null != param ? param : (null == this.param ? new WeakHashMap<String, Object>() : new WeakHashMap<String, Object>(this.param));
+      actualExecutionTime = new Date();
+      if (null != monitor) {
+        final Date _actualExecutionTime = actualExecutionTime;
+        new Thread(new Runnable() {
+
+          @Override
+          public void run() {
+            monitor.notificate(startTaskEvent.setHappendTime(_actualExecutionTime).setTaskId(taskId).setType(TaskEventType.START).setParam(_param));
+          }
+        }).start();
+      }
+
+      this.status = TaskStatusEnum.RUNNING;
+      logger.info("Task [{}] Start", this.name);
+      StopWatch stopWatch = new StopWatch();
+      stopWatch.start();
+      failPrintList = doJob(_param);
+      stopWatch.stop();
+      logger.info("Task [{}] End, the task cost time :  {}", this.name, stopWatch.getTime());
+      this.status = TaskStatusEnum.COMPLETED;
+    } catch (Throwable e) {
+      logger.error("Task failed", e);
+
+      this.status = TaskStatusEnum.FAILED;
+
+      failPrintList = Arrays.asList(ExceptionUtils.getStackTrace(e));
     }
+    completionTime = new Date();
+    nextExecutionTime = getTrigger().nextExecutionTime(triggerContext);
 
-    public Map<String, Object> getParam() {
-        return param;
+    if (null != failPrintList && this.canRetry) {
+      nextExecutionTime = Date.from(LocalDateTime.now().plus(this.retryAfter, ChronoUnit.SECONDS).atZone(ZoneId.systemDefault()).toInstant());
     }
+    if (null != monitor) {
+      final TaskStatusEnum taskStatus = this.status;
+      final List<String> _failPrintList = failPrintList;
+      final Date _nextExecutionTime = nextExecutionTime;
+      new Thread(new Runnable() {
 
-    public Task setParam(Map<String, Object> param) {
-        this.param = param;
-
-        return this;
-    }
-
-    public boolean isCanRetry() {
-        return canRetry;
-    }
-
-    public boolean isAlwaysFromBeginning() {
-        return alwaysFromBeginning;
-    }
-
-    public void setAlwaysFromBeginning(boolean alwaysFromBeginning) {
-        this.alwaysFromBeginning = alwaysFromBeginning;
-    }
-
-    public void setCanRetry(boolean canRetry) {
-        this.canRetry = canRetry;
-    }
-
-    public long getRetryAfter() {
-        return retryAfter;
-    }
-
-    /**
-     * retry after {retryAfter} seconds
-     * 
-     * @param retryAfter
-     */
-    public void setRetryAfter(long retryAfter) {
-        this.retryAfter = retryAfter;
-    }
-
-    public ScheduledExecutorService getExecutor() {
-        return executor;
-    }
-
-    public void setExecutor(ScheduledExecutorService executor) {
-        this.executor = executor;
-    }
-
-    public TaskStatusEnum getStatus() {
-        return status;
-    }
-
-    public Task setStatus(TaskStatusEnum status) {
-        this.status = status;
-
-        return this;
-    }
-
-    public String getId() {
-        return id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public TaskTriggerContext getTriggerContext() {
-        return triggerContext;
-    }
-
-    public BaseTrigger getTrigger() {
-        return triggerContext.getTrigger();
-    }
-
-    public ScheduledFuture<?> schedule() {
-        synchronized (this.triggerContextMonitor) {
-            Date scheduledExecutionTime = this.triggerContext.getTrigger().nextExecutionTime(this.triggerContext);
-            if (scheduledExecutionTime == null) {
-                return null;
-            }
-            long initialDelay = scheduledExecutionTime.getTime() - System.currentTimeMillis();
-
-            logger.info("Schedule task [{}] run @ {}", this.name, scheduledExecutionTime.toString());
-            this.currentFuture = this.executor.schedule(this, initialDelay, TimeUnit.MILLISECONDS);
-            return this;
+        @Override
+        public void run() {
+          monitor.notificate(new TaskEvent().setHappendTime(new Date()).setTaskId(taskId)
+              .setType(TaskStatusEnum.COMPLETED == taskStatus ? TaskEventType.COMPELETE : TaskEventType.FAIL).setFailPrintList(_failPrintList)
+              .setNextExecutionTime(_nextExecutionTime).setStartTime(startTaskEvent.getHappendTime()).setParam(startTaskEvent.getParam()));
         }
+      }).start();
     }
 
-    private void _run(Map<String, Object> param) {
-        Date nextExecutionTime = null, actualExecutionTime = null, completionTime = null;
-        List<String> failPrintList = null;
-        TaskEvent startTaskEvent = new TaskEvent();
-        try {
-            Map<String, Object> _param =
-                            null != param ? param : (null == this.param ? new WeakHashMap<String, Object>() : new WeakHashMap<String, Object>(
-                                            this.param));
-            actualExecutionTime = new Date();
-            if (null != monitor) {
-                monitor.notificate(startTaskEvent.setHappendTime(actualExecutionTime).setTaskId(this.id).setType(TaskEventType.START)
-                                .setParam(_param));
-            }
-
-            this.status = TaskStatusEnum.RUNNING;
-            logger.info("Task [{}] Start", this.name);
-            StopWatch stopWatch = new StopWatch();
-            stopWatch.start();
-            failPrintList = doJob(_param);
-            stopWatch.stop();
-            logger.info("Task [{}] End, the task cost time :  {}", this.name, stopWatch.getTime());
-            this.status = TaskStatusEnum.COMPLETED;
-        } catch (Throwable e) {
-            logger.error("Task failed", e);
-
-            this.status = TaskStatusEnum.FAILED;
-
-            failPrintList = Arrays.asList(ExceptionUtils.getStackTrace(e));
-        }
-        completionTime = new Date();
-        nextExecutionTime = getTrigger().nextExecutionTime(triggerContext);
-
-        if (null != failPrintList && this.canRetry) {
-            nextExecutionTime = Date.from(LocalDateTime.now().plus(this.retryAfter, ChronoUnit.SECONDS).atZone(ZoneId.systemDefault()).toInstant());
-        }
-        if (null != monitor) {
-            monitor.notificate(new TaskEvent().setHappendTime(new Date()).setTaskId(this.id)
-                            .setType(TaskStatusEnum.COMPLETED == this.status ? TaskEventType.COMPELETE : TaskEventType.FAIL)
-                            .setFailPrintList(failPrintList).setNextExecutionTime(nextExecutionTime).setStartTime(startTaskEvent.getHappendTime())
-                            .setParam(startTaskEvent.getParam()));
-        }
-
-        synchronized (this.triggerContextMonitor) {
-            this.triggerContext.update(nextExecutionTime, actualExecutionTime, completionTime);
-            if (!this.currentFuture.isCancelled()) {
-                schedule();
-            }
-        }
+    synchronized (this.triggerContextMonitor) {
+      this.triggerContext.update(nextExecutionTime, actualExecutionTime, completionTime);
+      if (!this.currentFuture.isCancelled()) {
+        schedule();
+      }
     }
+  }
 
-    public abstract List<String> doJob(Map<String, Object> param);
+  public abstract List<String> doJob(Map<String, Object> param);
 
-    @Override
-    public void run() {
-        _run(null);
+  @Override
+  public void run() {
+    _run(null);
+  }
+
+
+  @Override
+  public long getDelay(TimeUnit unit) {
+    ScheduledFuture<?> curr;
+    synchronized (this.triggerContextMonitor) {
+      curr = this.currentFuture;
     }
+    return curr.getDelay(unit);
+  }
 
-
-    @Override
-    public long getDelay(TimeUnit unit) {
-        ScheduledFuture<?> curr;
-        synchronized (this.triggerContextMonitor) {
-            curr = this.currentFuture;
-        }
-        return curr.getDelay(unit);
+  @Override
+  public int compareTo(Delayed other) {
+    if (this == other) {
+      return 0;
     }
+    long diff = getDelay(TimeUnit.MILLISECONDS) - other.getDelay(TimeUnit.MILLISECONDS);
+    return (diff == 0 ? 0 : ((diff < 0) ? -1 : 1));
+  }
 
-    @Override
-    public int compareTo(Delayed other) {
-        if (this == other) {
-            return 0;
-        }
-        long diff = getDelay(TimeUnit.MILLISECONDS) - other.getDelay(TimeUnit.MILLISECONDS);
-        return (diff == 0 ? 0 : ((diff < 0) ? -1 : 1));
+  @Override
+  public boolean cancel(boolean mayInterruptIfRunning) {
+    synchronized (this.triggerContextMonitor) {
+      return this.currentFuture.cancel(mayInterruptIfRunning);
     }
+  }
 
-    @Override
-    public boolean cancel(boolean mayInterruptIfRunning) {
-        synchronized (this.triggerContextMonitor) {
-            return this.currentFuture.cancel(mayInterruptIfRunning);
-        }
+  @Override
+  public boolean isCancelled() {
+    synchronized (this.triggerContextMonitor) {
+      return this.currentFuture.isCancelled();
     }
+  }
 
-    @Override
-    public boolean isCancelled() {
-        synchronized (this.triggerContextMonitor) {
-            return this.currentFuture.isCancelled();
-        }
+  @Override
+  public boolean isDone() {
+    synchronized (this.triggerContextMonitor) {
+      return this.currentFuture.isDone();
     }
+  }
 
-    @Override
-    public boolean isDone() {
-        synchronized (this.triggerContextMonitor) {
-            return this.currentFuture.isDone();
-        }
+  @Override
+  public Object get() throws InterruptedException, ExecutionException {
+    ScheduledFuture<?> curr;
+    synchronized (this.triggerContextMonitor) {
+      curr = this.currentFuture;
     }
+    return curr.get();
+  }
 
-    @Override
-    public Object get() throws InterruptedException, ExecutionException {
-        ScheduledFuture<?> curr;
-        synchronized (this.triggerContextMonitor) {
-            curr = this.currentFuture;
-        }
-        return curr.get();
+  @Override
+  public Object get(long timeout, TimeUnit unit) throws InterruptedException, ExecutionException, TimeoutException {
+    ScheduledFuture<?> curr;
+    synchronized (this.triggerContextMonitor) {
+      curr = this.currentFuture;
     }
-
-    @Override
-    public Object get(long timeout, TimeUnit unit) throws InterruptedException, ExecutionException, TimeoutException {
-        ScheduledFuture<?> curr;
-        synchronized (this.triggerContextMonitor) {
-            curr = this.currentFuture;
-        }
-        return curr.get(timeout, unit);
-    }
+    return curr.get(timeout, unit);
+  }
 }
